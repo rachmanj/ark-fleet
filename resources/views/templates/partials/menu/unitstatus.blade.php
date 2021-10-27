f5b32473a04f8252759d27487e386f9f0507a327
rachmanj <rachmanj@gmail.com>
2021-10-27 08:58:28 +0800

    minor update

--- resources/views/templates/partials/menu/unitstatus.blade.php
+++ resources/views/templates/partials/menu/unitstatus.blade.php
@@ -1,6 +1,6 @@
 <li class="nav-item">
   <a href="{{ route('unitstatuses.index') }}" class="nav-link {{ request()->is('unitstatuses') || request()->is('unitstatuses/*') ? 'active' : '' }}">
-    <i class="far fa-file-alt"></i>
+    <i class="far fa-circle nav-icon"></i>
     <p>
       Unit Status
     </p>
